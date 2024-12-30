567a4f958399f7d7b762e57f9621e2e52d5cb099
MarjolijnMuller <polle.marjolijn@gmail.com>
2024-12-30 15:17:16 +0100

    start

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+function Menu(props) {
+    <nav>
+        <ul>
+        <li>home</li>
+        <li>zoeken</li>
+        <li>Mijn favorieten</li>
+        <li>Account</li>
+        </ul>
+        <button type="button">Inloggen</button>
+    </nav>
+}
+
+export default Menu;
